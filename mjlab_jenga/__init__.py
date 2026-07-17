f0f0868bf1a78d9d927ee1c53fcd40634d55d463
Timur Uttsal <timuruttsal@gmail.com>
2026-07-17 15:12:59 +0200

    random block choose

--- mjlab_jenga/__init__.py
+++ mjlab_jenga/__init__.py
@@ -9,3 +9,15 @@ register_mjlab_task(
     play_env_cfg=jenga_env_cfg(play=True),
     rl_cfg=jenga_ppo_runner_cfg(),
 )
+
+# Random-block worker: each episode randomly picks a block, teleports the hook in
+# front of it, policy learns to push it out. Built on Boris's main config + a
+# JengaPushCommand. Launch: train.py / play.py Mjlab-Jenga-RandomBlock
+from mjlab_jenga import jenga_random_block_cfg as _randblock  # noqa: E402
+
+register_mjlab_task(
+    task_id="Mjlab-Jenga-RandomBlock",
+    env_cfg=_randblock.jenga_env_cfg(),
+    play_env_cfg=_randblock.jenga_env_cfg(play=True),
+    rl_cfg=_randblock.jenga_ppo_runner_cfg(),
+)
